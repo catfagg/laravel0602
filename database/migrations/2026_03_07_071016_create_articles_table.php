7e49776c889826a4f3282881a9291f9056f73c30
gdfudfiuodfgdfiuogdiou <dfgdyuruye@fsduiif.com>
2026-03-06 23:33:12 -0800

    abdcaoe

---
+++ database/migrations/2026_03_07_071016_create_articles_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->string('name');
+            $table->text('text');
+            $table->date('created_at');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
