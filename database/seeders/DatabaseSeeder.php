2dafc5d8c87bd5be2245504e4bf35bf61e1d69be
gdfudfiuodfgdfiuogdiou <dfgdyuruye@fsduiif.com>
2026-03-09 22:27:19 -0700

    -

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,17 +5,43 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         // User::factory(10)->create();
+
+        DB::table('posts')->insert([
+				[
+					'title' => 'title 1',
+					'slug'  => 'post-1',
+					'text'  => 'text text text 1',
+				],
+				[
+					'title' => 'title 2',
+					'slug'  => 'post-2',
+					'text'  => 'text text text 2',
+				],
+				[
+					'title' => 'title 3',
+					'slug'  => 'post-3',
+					'text'  => 'text text text 3',
+                ],
+                [
+                    'title' => Str::random(10),
+				    'slug'  => Str::random(10),
+				    'text'  => Str::random(50),
+
+				],
+			]);
+
+        DB::table('domains')->insert([
+				'name' => Str::random(10) . '.com',
+			]);
 
         User::factory()->create([
             'name' => 'Test User',
